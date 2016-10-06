89dc1087a5be74bfb1382b9ebfdfa0d549a5d13e
Shawn Wu <swu@fitbit.com>
2016-10-06 09:44:15 -0700

    add order views

---
+++ BuzzNowMobile/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  
+//
+//  Created by Shawn Wu on 10/5/16.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class Product: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
